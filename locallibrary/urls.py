ddc162bd1b532a87d05c791bf5bbea84e67c7e25
Rim <rimkarimov20@gmail.com>
2020-06-15 08:39:43 +0500

    added urlpatterns

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -18,5 +18,22 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+# Используйте include() чтобы добавлять URL из каталога приложения
+from django.urls import include
+
+urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
+# Добавьте URL соотношения, чтобы перенаправить запросы с корневового URL, на URL приложения
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+# Используйте static() чтобы добавить соотношения для статических файлов
+# Только на период разработки
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
