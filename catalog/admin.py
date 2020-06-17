60e1b28e4147ae9548cfb74789bbcec08ff20256
Rim <rimkarimov20@gmail.com>
2020-06-17 09:36:18 +0500

    Added models and admin panels settings

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book, Genre, BookInstance, Language
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth' , 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
