4b47b83528b1c4b3f6b127c1559a61a2514dfafd
Rim <rimkarimov20@gmail.com>
2020-06-18 10:58:30 +0500

    added templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre, Language
 
-# Create your views here.
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genre = Genre.objects.all().count
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+            'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genre': num_genre},
+    )
