c9c038267fbefc37207cda993ce3f1ef149f2115
Rim <rimkarimov20@gmail.com>
2020-06-23 12:51:55 +0500

    Staff page

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^authors/$', views.AuthorListView.as_view(), name = 'authors'),
-    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail')
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my_borrowed'),
+    url(r'^loanedbooks/$', views.AllLoanedBooksListView.as_view(), name='loaned-books'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -43,3 +43,29 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user. 
+    """
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+class AllLoanedBooksListView(generic.ListView, PermissionRequiredMixin):
+    permission_required = 'can_mark_returned'
+    model = BookInstance
+    paginate_by = 10
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
